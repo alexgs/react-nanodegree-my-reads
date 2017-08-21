cd5e10fe349a5d9605992df0a497eeecd9c8c0c3
Phil Gates-Shannon <phil.gs@gmail.com>
2017-08-21 09:58:49 -0400

    Add "shelf changer" component

--- src/home/Book.js
+++ src/home/Book.js
@@ -1,8 +1,8 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import ShelfChanger from './ShelfChanger';
 
 function Book( props ) {
-    // TODO Convert shelf-changer to a React component
     // TODO Make self-changer actually do something
     const bookCoverStyle = {
         backgroundImage: `url("${props.coverUrl}")`,
@@ -14,15 +14,7 @@ function Book( props ) {
         <div className="book">
             <div className="book-top">
                 <div className="book-cover" style={ bookCoverStyle }/>
-                <div className="book-shelf-changer">
-                    <select>
-                        <option value="none" disabled>Move to...</option>
-                        <option value="currentlyReading">Currently Reading</option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
-                    </select>
-                </div>
+                <ShelfChanger/>
             </div>
             <div className="book-title">{ props.title }</div>
             <div className="book-authors">{ props.author }</div>
---
+++ src/home/ShelfChanger.js
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+function ShelfChanger( props ) {
+    return (
+        <div className="book-shelf-changer">
+            <select>
+                <option value="none" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+            </select>
+        </div>
+    );
+}
+
+ShelfChanger.propTypes = {
+
+};
+
+export default ShelfChanger;
