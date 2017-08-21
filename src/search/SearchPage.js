817a100d1ee679752c0ada3c41063400515bf75f
Phil Gates-Shannon <phil.gs@gmail.com>
2017-08-21 10:19:52 -0400

    Connect Search page to app state

--- src/search/SearchPage.js
+++ src/search/SearchPage.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import { Link } from 'react-router-dom'
 
@@ -7,16 +8,12 @@ function SearchPage( props ) {
             <div className="search-books-bar">
                 <Link className="close-search" to="/">Close</Link>
                 <div className="search-books-input-wrapper">
-                    {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                    <input type="text" placeholder="Search by title or author"/>
-
+                    <input
+                        type="text"
+                        placeholder="Search by title or author"
+                        value={ props.query }
+                        onChange={ event => props.updateQuery( event.target.value )}
+                    />
                 </div>
             </div>
             <div className="search-books-results">
@@ -25,5 +22,11 @@ function SearchPage( props ) {
         </div>
     );
 }
+
+SearchPage.propTypes = {
+    query: PropTypes.string.isRequired,
+    searchResults: PropTypes.array.isRequired,
+    updateQuery: PropTypes.func.isRequired
+};
 
 export default SearchPage;
